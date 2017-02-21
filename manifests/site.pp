8187efd647a8543f3d076ff1f9b6c5ee5eb2023f
ben573313 <benjamin.steele.ctr@gmail.com>
2017-02-21 14:30:08 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,6 +45,6 @@ node default {
   include role::classroom
   exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
       path => '/usr/local/bin',
-      creates => 'etc/motd',
+      creates => '/etc/motd',
     }
 }
