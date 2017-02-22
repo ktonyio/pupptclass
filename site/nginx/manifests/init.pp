2828cd6f61161923fef4c6eade1f93b55eb3f3c8
Jack Hilary <Jack.Hilary.CTR@fcc.gov>
2017-02-22 11:37:37 -0500

    12.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,38 +1,33 @@
 class nginx {
-  package { 'nginx':
-    ensure => latest
-  }
- file { '/var/www':
-  ensure => directory,
-  owner => 'root',
-  group => 'root',
-  mode => '0755'
-  }
-  file { '/var/www/index.html':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-    source => 'puppet:///modules/nginx/index.html'
-  }
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx']
-  }
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///modules/nginx/default.conf'
-  }
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf']
-  }
+File {
+owner => 'root',
+group => 'root',
+mode => '0664',
+}
+package { 'nginx':
+ensure => present,
+}
+file { [ '/var/www', '/etc/nginx/conf.d' ]:
+ensure => directory,
+}
+file { '/var/www/index.html':
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { '/etc/nginx/nginx.conf':
+ensure => file,
+source => 'puppet:///modules/nginx/nginx.conf',
+require => Package['nginx'],
+notify => Service['nginx'],
+}
+file { '/etc/nginx/conf.d/default.conf':
+ensure => file,
+source => 'puppet:///modules/nginx/default.conf',
+notify => Service['nginx'],
+require => Package['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
 }
