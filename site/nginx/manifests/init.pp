5e790f299f6ed5725d9551d24e428ce5c9563542
welf28 <welf.dixon@fcc.gov>
2017-02-22 11:31:25 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,32 +2,24 @@ class nginx {
   package { 'nginx':
     ensure => latest,
   }
-  file { '/var/www':
-    ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
-  }
-  file { '/var/www/index.html':
+  File {
     ensure => file,
     owner => 'root',
     group => 'root',
     mode => '0644',
+  }
+  file { '/var/www':
+    ensure => directory,
+    mode => '0755',
+  }
+  file { '/var/www/index.html':
     source => 'puppet:///modules/nginx/index.html',
   }
   file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
