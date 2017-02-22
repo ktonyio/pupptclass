47a3e66585d90339604d2decc58d9142684ca5c9
kumar1078 <kumar.nainala@gmail.com>
2017-02-22 13:10:25 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,15 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+}
+}
   include role::classroom
   include ::skeleton
   include ::nginx
