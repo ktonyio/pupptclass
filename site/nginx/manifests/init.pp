59b3deb3cd042cdb7ac4c9aa2b261fb41f6df737
ben573313 <benjamin.steele.ctr@gmail.com>
2017-02-22 11:37:09 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -11,7 +11,7 @@ class nginx {
     ensure => directory,
   
   }
-  file { '/var/www/index.html'
+  file { '/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
