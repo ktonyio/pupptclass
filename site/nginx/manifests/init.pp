2e190a0b1a9df2c6c7dc5375a67238d344b06749
ktonyio <ktonyio@yahoo.com>
2017-02-22 11:08:17 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,40 @@
 
+class nginx {
+  package { 'nginx':
+    ensure => latest,
+  }
+  file { '/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
+  }
+}
