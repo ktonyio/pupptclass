ac1d56a027101907db3d79dc3844322b171f6f5f
ben573313 <benjamin.steele.ctr@gmail.com>
2017-02-22 11:36:48 -0500

    refacter

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,27 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   package { 'nginx':
     ensure => latest,
   }
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
+  
   }
-  file { '/var/www/index.html':
+  file { '/var/www/index.html'
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/index.html',
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
