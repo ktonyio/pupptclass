1060e574d536108dae40d1e1fed36d8ed7d9e82e
root <root@master.puppetlabs.vm>
2017-02-22 19:50:56 +0000

    lab 13.2 - homework

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,32 @@
 class nginx {
+  case $::osfamily {
+    'redhat', 'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '${docroot}'
+      $logdir = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs' 
+    }
+    default : {
+      fail("Unsupported OS! (${::osfamily})")
+    }
+  }
+
+  $user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
+
   File {
     owner => 'root',
     group => 'root',
@@ -7,26 +35,30 @@ class nginx {
   package { 'nginx':
     ensure => latest,
   }
-  file { '/var/www':
+  file { [$docroot, "${confdir}/conf.d"]:
     ensure => directory,
   }
-  file { '/var/www/index.html':
+  file { "${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
+  file { "${docroot}/nginx.conf":
+    ensure       => file,
+    content      => epp('nginx/nginx.conf.epp', {
+        user     => $user,
+        logdir   => $logdir,
+        confdir => $confdir,
+      }),
     require => Package['nginx'],
   }
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
+  file { "${docroot}/conf.d/default.conf":
+    ensure      => file,
+    content     => epp('nginx/default.conf.epp', {docroot => $docroot}),
     require => Package['nginx'],
   }
   service { 'nginx':
     ensure => running,
     enable => true,
-    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
+    subscribe => File["${docroot}/nginx.conf", "${docroot}/conf.d/default.conf"],
   }
 }
