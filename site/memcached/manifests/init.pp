f92326f81e5724edf704c5a28e8e3c9c81fd0eba
welf28 <welf.dixon@fcc.gov>
2017-02-22 10:42:17 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,18 +1,21 @@
-package { 'memcached':
-  ensure => present,
-}
+class memcached {
 
-file { '/etc/sysconfig/memcached':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  source => 'puppet:///modules/memecached/memcached',
-  require => Package['memcached'],
-  notify => Service['memcached',
-}
+  package { 'memcached':
+    ensure => present,
+  }
 
-service { 'memcached':
-  ensure => running,
-  enable => true,
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/memecached/memcached',
+    require => Package['memcached'],
+    notify => Service['memcached',
+  }
+
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+  }
 }
