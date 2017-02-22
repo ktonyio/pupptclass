56f4e0139c9767c106fb5bd90c886a95002ebde2
billfergus <bill.fergus@fcc.gov>
2017-02-22 11:36:57 -0500

    Use Resource Default to reduce code duplication - File

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,34 +1,28 @@
 class nginx {
+  # common practice to keep Resource Defaults near top of puppet file
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   package { 'nginx':
     ensure => present,
   }
   file { '/var/www/':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
   }
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/index.html',
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     # notifying the service from the file, simmilar to subscribe
