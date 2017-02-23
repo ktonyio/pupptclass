1fad63c1f73a82493b1ac827ecf6a71067c39e3b
kumar1078 <kumar.nainala@gmail.com>
2017-02-23 11:12:11 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,66 +1,51 @@
-class nginx {
-  case $::osfamily {
-    'redhat', 'debian': {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-   
-    }
-    'windows': {
-      $package = 'nginx-service'
-      $owner = 'Administrator'
-      $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
-      $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs' 
-    }
-    default : {
-      fail("Unsupported OS! (${::osfamily})")
-    }
-  }
-
-  $user = $::osfamily ? {
-    'redhat'  => 'nginx',
-    'debian'  => 'www-data',
-    'windows' => 'nobody',
-  }
-
-  File {
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-  }
-  package { 'nginx':
-    ensure => latest,
-  }
-  file { [$docroot,  "${confdir}/conf.d"]:
-    ensure => directory,
-  }
-  file { "${docroot}/index.html":
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  file { "${confdir}/nginx.conf":
-    ensure       => file,
-    content      => epp('nginx/nginx.conf.epp', {
-        user     => $user,
-        logdir   => $logdir,
-        confdir => $confdir,
-        piddir => $piddir,
-      }),
-    require => Package['nginx'],
-  }
-  file { "${confdir}/conf.d/default.conf":
-    ensure      => file,
-    content     => epp('nginx/default.conf.epp', {docroot => $docroot}),
-    require => Package['nginx'],
-  }
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => File["${confdir}/nginx.conf", "${confdir}/conf.d/default.conf"],
-  }
+class nginx (
+$package = $nginx::params::package,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir = $nginx::params::logdir,
+$user = $nginx::params::user,
+$port = $nginx::params::port
+) inherits nginx::params {
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
 }
+package { $package:
+ensure => present,
+}
+file { [ $docroot, "${confdir}/conf.d" ]:
+ensure => directory,
+}
+file { "${docroot}/index.html":
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { "${confdir}/nginx.conf":
+ensure => file,
+content => epp('nginx/nginx.conf.epp',
+{
+user => $user,
+confdir => $confdir,
+logdir => $logdir,
+}),
+notify => Service['nginx'],
+}
+file { "${confdir}/conf.d/default.conf":
+ensure => file,
+content => epp('nginx/default.conf.epp',
+{
+port => $port,
+docroot => $docroot,
+}),
+notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
+}
+Lab 17.2: Proposed Solution
+VirtualFundamentals
