79c438e38cbf89f5b1ffb541483c689f9f38a2a0
billfergus <bill.fergus@fcc.gov>
2017-02-23 11:28:03 -0500

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,7 +1,7 @@
 class nginx::params {
-  $owner => 'root',
-  $group => 'root',
-  $docroot => '/var/www/docroot',
+  $owner = 'root',
+  $group = 'root',
+  $docroot = '/var/www/docroot',
   $log_dir = '/var/log/nginx',
   $port = '80',
 }
