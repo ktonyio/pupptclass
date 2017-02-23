d854b76ff820ca5740a88ae8cfae0da108547194
ktonyio <ktonyio@yahoo.com>
2017-02-23 09:36:25 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,1 +1,17 @@
 
+define users::managed_user (
+  $username = $title,
+  $group = $title,
+  $homedir = "/home/${username}",
+) {
+  user { $username:
+    ensure => present,
+    home => $homedir,
+    managehome => true,
+  }
+  file { $homedir:
+    ensure => directory,
+    owner => $username,
+    group => $group,
+  }
+}
