a4117e10547a50b0b0f5dc42682057d8fe681bf3
ktonyio <ktonyio@yahoo.com>
2017-02-23 11:37:00 -0500

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,1 +1,29 @@
-
+class nginx::params {
+  case $::osfamily {
+    'redhat', 'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs' 
+    }
+    default : {
+      fail("Unsupported OS! (${::osfamily})")
+    }
+  }
+  $user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
+  $port = '80'
+}
