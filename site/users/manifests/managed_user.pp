ea4a34bee0f9fa8eabbba7c64705d7480e4743fb
billfergus <bill.fergus@fcc.gov>
2017-02-23 09:35:59 -0500

    add colon

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -4,7 +4,7 @@ define users::managed_user (
   user { $title:
     ensure => present,
   }
-  file { "/home/${title}"
+  file { "/home/${title}":
     ensure => directory,
     owner => $title,
     group => $group,
