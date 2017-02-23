69dbc094dbc9befde777f21f136e7f9cc7cb86af
Drew Nase <dnasecs@gmail.com>
2017-02-23 09:22:12 -0500

    lab 15.1

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,16 @@
+define users::managed_user (
+  $username = $title,
+  $group = $title,
+  $homedir = "/home/${username}",
+) {
+  user { $username:
+    ensure => present,
+    home => $homedir,
+    managehome => true,
+  }
+  file { $homedir:
+    ensure => directory,
+    owner => $username,
+    group => $group,
+  }
+}
