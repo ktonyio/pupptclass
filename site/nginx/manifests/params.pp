75e64bfda6532431f2aa08ab32d6ae2e0d9e3a46
Drew Nase <dnasecs@gmail.com>
2017-02-23 11:21:26 -0500

    lab 17.2

---
+++ site/nginx/manifests/params.pp
@@ -0,0 +1,32 @@
+class nginx::params {
+  
+  case $::osfamily {
+    'redhat', 'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $default_docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $default_docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs' 
+    }
+    default : {
+      fail("Unsupported OS! (${::osfamily})")
+    }
+  }
+
+  $user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
+  
+  $docroot = pick($root, $default_docroot)
+}
