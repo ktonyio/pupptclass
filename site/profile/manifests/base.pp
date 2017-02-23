c45c6340dd2c0df68b6b1072e401b84a2b43bd37
welf28 <welf.dixon@fcc.gov>
2017-02-23 10:04:12 -0500

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,5 @@
 class profile::base {
 #  notify { "Hello, my name is ${::hostname}": }
-  $hostname = hiera('hostname', 'not working'),
-  notify { "Hello, my name is hiera('hostname', 'not working')": }
+  $hostname = hiera('hostname', 'not working')
+  notify { "Hello, my name is ${hostname}": }
 }
